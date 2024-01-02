02560e75c188a1a26e6cbd4bdfe61ee55173fa0f
code-a-man <43219246+code-a-man@users.noreply.github.com>
2024-01-03 02:10:45 +0300

    Update Footer component styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className="bg-white dark:bg-gray-800 w-full py-8">
+		<footer className="bg-white dark:bg-gray-800 w-full py-8 mt-40">
 			<div className="max-w-screen-xl mx-auto px-4">
 				<div className="flex justify-center md:justify-between flex-wrap md:flex-nowrap items-center">
 					<div className="flex flex-wrap justify-center md:justify-start -mb-4 -mx-4">
