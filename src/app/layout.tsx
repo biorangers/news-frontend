5a8536dd4cc664cae1f22861ea0324097aec3d49
code-a-man <43219246+code-a-man@users.noreply.github.com>
2024-01-03 02:09:17 +0300

    Add background images to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Providers } from "./providers";
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 const inter = Inter({ subsets: ['latin'] })
-
+import { Image } from "@nextui-org/react";
 export const metadata: Metadata = {
   title: 'HABERTO',
   description: 'En güncel haberler',
@@ -19,13 +19,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <main className="dark text-foreground bg-background">
+        <main className="dark text-foreground bg-background relative">
           <Providers>
             <NavBar />
             {children}
             <Footer />
           </Providers>
         </main>
+        <div
+          aria-hidden="true"
+          className="fixed hidden dark:md:block dark:opacity-70 scale-110 -translate-y-6 blur-lg -bottom-[40%] -left-[20%] z-0"
+        >
+          <Image removeWrapper alt="docs left background" src="/gradients/docs-left.png" />
+        </div>
+        <div
+          aria-hidden="true"
+          className="fixed hidden dark:md:block dark:opacity-70 scale-110 -translate-y-6 blur-lg -top-[80%] -right-[60%] 2xl:-top-[60%] 2xl:-right-[45%] z-0 rotate-12"
+        >
+          <Image removeWrapper alt="docs right background" src="/gradients/docs-right.png" />
+        </div>
       </body>
     </html >
   )
