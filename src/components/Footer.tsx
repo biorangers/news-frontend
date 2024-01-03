561284d7aa35944659efd8c2074cc7082b20cd47
code-a-man <43219246+code-a-man@users.noreply.github.com>
2024-01-03 22:26:22 +0300

    feat: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,11 @@
 export default function Footer() {
 	return (
-		<footer className="bg-white dark:bg-gray-800 w-full py-8 mt-40">
-			<div className="max-w-screen-xl mx-auto px-4">
-				<div className="flex justify-center md:justify-between flex-wrap md:flex-nowrap items-center">
-					<div className="flex flex-wrap justify-center md:justify-start -mb-4 -mx-4">
-						<div className="mb-4 mx-4">
-							asdasdasd
-						</div>
-						<div className="mb-4 mx-4">
-							asdasdasd
-						</div>
-					</div>
+		<footer className="w-full py-8 mt-40">
+			< div className="max-w-screen-xl mx-auto px-4" >
+				<div className="flex justify-center items-center">
+					<p className="text-lg font-bold">HABERTO © 2024</p>
 				</div>
-			</div>
-		</footer>
+			</div >
+		</footer >
 	);
 }
