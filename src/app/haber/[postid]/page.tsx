ae1f91d9f5e76a82efa61b529dea1711c72ed306
code-a-man <43219246+code-a-man@users.noreply.github.com>
2024-01-04 08:09:20 +0300

    add viewer to news page

--- src/app/haber/[postid]/page.tsx
+++ src/app/haber/[postid]/page.tsx
@@ -3,12 +3,14 @@ import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 import { useRouter } from 'next/router'
 import axios from 'axios'
 import AuthorCard from "@/components/AuthorCard";
+import Viewer from "@/components/Viewer";
 
 export default async function Page({ params }: { params: { postid: string } }) {
 
 	const res = await fetch(`http://localhost:5074/api/News/GetNewsById/${params.postid}`);
 	const newsData = (await res.json())[0]
-	const author = await (await axios.get(`http://localhost:5074//api/User/GetAuthorById/${newsData.authorID}`)).data[0];
+	const authorData = await (await axios.get(`http://localhost:5074/api/User/GetAllAuthors`)).data
+	const author = authorData.find((author: any) => author.authorId === newsData.authorID)
 	return (
 		<div className="container p-2 mt-2 prose max-w-[900px] prose-stone dark:prose-invert text-neutral-300 flex flex-col justify-center mx-auto">
 			<Image
@@ -21,6 +23,7 @@ export default async function Page({ params }: { params: { postid: string } }) {
 			<h1>{newsData.articleTitle}</h1>
 			<div dangerouslySetInnerHTML={{ __html: newsData.articleContent.replace(/\\/g, '').replace(/^"(.*)"$/, '$1') }} />
 			<AuthorCard author={author} />
+			<Viewer articleId={params.postid} />
 		</div>
 	);
 }
